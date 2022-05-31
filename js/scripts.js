c14211d1812e0d05e4de7d32a166d1f859f2fea5
Alex Shevlin <alexshevlin1@gmail.com>
2022-05-31 12:05:31 -0700

    Co-authored By: Daniel Ware <waredanielb@gmail.com>.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,27 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function toCelcius(number1) {
+  return (number1 - 32) / 1.8;
+}
+
+function toFahrenheit(number1) {
+  return (number1 * 1.8) + 32;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+const result = toFahrenheit(number1, number2);
 alert(result);
