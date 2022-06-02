e07ebacd2d6ed0e320f65a8a42c18e735b0482e7
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-02 09:35:29 -0700

    Add user interface logic for form submission to output result to webpage.
    Co-authored-by: Joseph Jack <Josephwjack@users.noreply.github.com>
    Co-authored-by: PJ Perry <pjperry28@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,15 +14,12 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-function toCelcius(number1) {
-  return (number1 - 32) / 1.8;
-}
-
-function toFahrenheit(number1) {
-  return (number1 * 1.8) + 32;
-}
-
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = toFahrenheit(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
